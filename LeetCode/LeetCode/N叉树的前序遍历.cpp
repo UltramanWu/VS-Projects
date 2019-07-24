8104462629ac2eb7b97441888d0bae85438a9615
UltramanWu <792371481@qq.com>
2019-07-24 11:15:10 +0800

    LeetCode first change

--- LeetCode/LeetCode/N叉树的前序遍历.cpp
+++ LeetCode/LeetCode/N叉树的前序遍历.cpp
@@ -1,54 +1,66 @@
-#include <iostream>
-#include <vector>
-#include <string>
-#include <algorithm>
-using namespace std;
+//#include <iostream>
+//#include <vector>
+//#include <string>
+//#include <stack>
+//#include <algorithm>
+//using namespace std;
+//
+//class Node {
+//public:
+//	int val;
+//	vector<Node*> children;
+//
+//	Node() {}
+//
+//	Node(int _val, vector<Node*> _children) {
+//		val = _val;
+//		children = _children;
+//	}
+//};
+//
+//vector<int> preorder(Node* root);
+//int main()
+//{
+//
+//	system("pause");
+//	return 0;
+//}
 
-class Node {
-public:
-	int val;
-	vector<Node*> children;
-
-	Node() {}
-
-	Node(int _val, vector<Node*> _children) {
-		val = _val;
-		children = _children;
-	}
-};
-
-vector<int> preorder(Node* root);
-int main()
-{
-
-	system("pause");
-	return 0;
-}
-
-/* 递归实现 */
-void PreSubOrder(Node *root, vector<int> &pre){
-	if (root == NULL)
-		return;
-	pre.push_back(root->val);
-	for (int i = 0; i < root->children.size(); i++)
-		if (root->children[i])
-			PreSubOrder(root->children[i], pre);
-}
-
-/* N叉树的前序遍历 */
-vector<int> preorder(Node* root) {
-	if (root == NULL)
-		return{};
-	vector<int> Pre;
-	PreSubOrder(root, Pre);
-
-	return Pre;
-}
+///* 递归实现 */
+//void PreSubOrder(Node *root, vector<int> &pre){
+//	if (root == NULL)
+//		return;
+//	pre.push_back(root->val);
+//	for (int i = 0; i < root->children.size(); i++)
+//		if (root->children[i])
+//			PreSubOrder(root->children[i], pre);
+//}
+//
+///* N叉树的前序遍历 */
+//vector<int> preorder(Node* root) {
+//	if (root == NULL)
+//		return{};
+//	vector<int> Pre;
+//	PreSubOrder(root, Pre);
+//
+//	return Pre;
+//}
 
 /* 非递归实现 */
-vector<int> preorder(Node* root){
-	if (root == NULL)
-		return{};
+/* 原理：按照子树遍历的反向顺序按照“先进后出”的原理先对其进行压栈，先压栈最右边的子树，
+再压栈最左边的子树。当弹出时先弹出左子树，按照这种次序就可以实现前序遍历。 */
+//vector<int> preorder(Node* root) {
+//	vector<int> ret;
+//	stack<Node* > s;
+//	s.push(root);
+//	while (!s.empty()){
+//		root = s.top(); s.pop();
+//		if (!root) continue;
+//		ret.push_back(root->val);
+//		for (int i = root->children.size() - 1; i >= 0; --i){
+//			s.push(root->children[i]);
+//		}
+//	}
+//	return ret;
+//}
 
-
-}
---
+++ LeetCode/LeetCode/二叉树的中序遍历.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+vector<int> inorderTraversal(TreeNode* root);
+
+int main()
+{
+	TreeNode* Root = new TreeNode(1);
+	TreeNode* Left = new TreeNode(2);
+	TreeNode* Right = new TreeNode(3);
+	Root->right = Left;
+	Left->left = Right;
+	vector<int> Result = inorderTraversal(Root);
+	system("pause");
+	return 0;
+}
+
+///* 递归实现 */
+//void inSubOrder(TreeNode *root, vector<int> &pre){
+//	if (root == NULL)
+//		return;
+//
+//	inSubOrder(root->left, pre);
+//	pre.push_back(root->val);
+//	inSubOrder(root->right, pre);
+//}
+//
+///* N叉树的前序遍历 */
+//vector<int> inorderTraversal(TreeNode* root) {
+//	if (root == NULL)
+//		return{};
+//	vector<int> Pre;
+//	inSubOrder(root, Pre);
+//
+//	return Pre;
+//}
+
+/* 非递归实现中序遍历 */
+/* 原理：按照子树遍历的反向顺序按照“先进后出”的原理先对其进行压栈，先压栈最右边的子树，
+再压栈最左边的子树。当弹出时先弹出左子树，按照这种次序就可以实现前序遍历。 */
+vector<int> inorderTraversal(TreeNode* root){
+	if (root == NULL)
+		return{};
+	stack<TreeNode* > S;  /* 堆栈 */
+	vector<int>  Pre;  /* 容器存放 */
+	while (root || !S.empty()){
+		while (root){
+			S.push(root);
+			root = root->left;
+		}
+		root = S.top(); S.pop();
+		Pre.push_back(root->val);
+		root = root->right;
+	}
+	return Pre;
+}
